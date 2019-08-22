dbf1e210ecbcfad3b3802c5cc797297914138572
Endel Dreyer <endel.dreyer@gmail.com>
2019-08-21 23:47:29 -0300

    update example

--- WebSocketExample/Connection.cs
+++ WebSocketExample/Connection.cs
@@ -7,51 +7,60 @@ using NativeWebSocket;
 
 public class Connection : MonoBehaviour
 {
-    WebSocket websocket;
+  WebSocket websocket;
 
-    // Start is called before the first frame update
-    async void Start()
+  // Start is called before the first frame update
+  async void Start()
+  {
+    websocket = new WebSocket("ws://echo.websocket.org");
+
+    websocket.OnOpen += () =>
     {
-      websocket = new WebSocket("wss://echo.websocket.org");
+      Debug.Log("Connection open!");
+    };
 
-      websocket.OnOpen += () =>
-      {
-        Debug.Log("Connection open!");
-      };
-
-      websocket.OnError += (e) =>
-      {
-        Debug.Log("Error! " + e);
-      };
-
-      websocket.OnClose += (e) =>
-      {
-        Debug.Log("Connection closed!");
-      };
-
-      websocket.OnMessage += (bytes) =>
-      {
-        // Reading a plain text message
-        var message = System.Text.Encoding.UTF8.GetString(bytes);
-        // Debug.Log("OnMessage! " + message);
-
-        // Sending bytes
-        websocket.Send(new byte[] { 10, 20, 30 });
-
-        // Sending plain text
-        websocket.SendText("plain text message");
-      };
-
-      await websocket.Connect();
-    }
-
-    // Update is called once per frame
-    void Update()
+    websocket.OnError += (e) =>
     {
-    }
+      Debug.Log("Error! " + e);
+    };
 
-    private async void OnApplicationQuit()
+    websocket.OnClose += (e) =>
     {
-      await websocket.Close();
+      Debug.Log("Connection closed!");
+    };
+
+    websocket.OnMessage += (bytes) =>
+    {
+      // Reading a plain text message
+      var message = System.Text.Encoding.UTF8.GetString(bytes);
+      Debug.Log("OnMessage! " + message);
+    };
+
+    // Keep sending messages at every 0.3s
+    InvokeRepeating("SendWebSocketMessage", 0.0f, 0.3f);
+
+    await websocket.Connect();
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+  }
+
+  async void SendWebSocketMessage()
+  {
+    if (websocket.State == WebSocketState.Open)
+    {
+      // Sending bytes
+      await websocket.Send(new byte[] { 10, 20, 30 });
+
+      // Sending plain text
+      await websocket.SendText("plain text message");
     }
+  }
+
+  private async void OnApplicationQuit()
+  {
+    await websocket.Close();
+  }
 }
